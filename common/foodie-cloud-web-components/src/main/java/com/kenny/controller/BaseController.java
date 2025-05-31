8d3ceef5d1b2f75f7a3dcf4ecec17fef0ecdd5d1
kennyyiu <kenny.kwyiu@gmail.com>
2025-05-31 18:59:58 +0800

    add BaseController

--- common/foodie-cloud-web-components/src/main/java/com/kenny/controller/BaseController.java
+++ common/foodie-cloud-web-components/src/main/java/com/kenny/controller/BaseController.java
@@ -13,14 +13,14 @@ public class BaseController {
     public static final Integer COMMON_PAGE_SIZE = 10;
     public static final Integer PAGE_SIZE = 20;
 
-    // 支付中心的调用地址
+    // Payment center call address
     String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";		// produce
 
-    // 微信支付成功 -> 支付中心 -> 天天吃货平台
-    //                       |-> 回调通知的url
+    // WeChat payment success -> Payment center -> Foodie platform
+    //                       |-> Callback notification url
     String payReturnUrl = "http://api.z.mukewang.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
 
-    // 用户上传头像的位置
+    // User avatar upload location
     public static final String IMAGE_USER_FACE_LOCATION = File.separator + "workspaces" +
                                                             File.separator + "images" +
                                                             File.separator + "foodie" +
@@ -33,13 +33,13 @@ public class BaseController {
 //    public MyOrdersService myOrdersService;
 //
 //    /**
-//     * 用于验证用户和订单是否有关联关系，避免非法用户调用
+//     * Used to verify the relationship between user and order to prevent unauthorized access
 //     * @return
 //     */
 //    public IMOOCJSONResult checkUserOrder(String userId, String orderId) {
 //        Orders order = myOrdersService.queryMyOrder(userId, orderId);
 //        if (order == null) {
-//            return IMOOCJSONResult.errorMsg("订单不存在！");
+//            return IMOOCJSONResult.errorMsg("Order does not exist!");
 //        }
 //        return IMOOCJSONResult.ok(order);
 //    }
