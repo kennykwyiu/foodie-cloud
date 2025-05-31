5d36b0547d171c0bff5da4b4b39503a30a5eaf45
kennyyiu <kenny.kwyiu@gmail.com>
2025-05-31 17:07:19 +0800

    add pojo

---
+++ common/foodie-cloud-shared-pojo/src/main/java/com/kenny/pojo/IMOOCJSONResult.java
@@ -0,0 +1,136 @@
+package com.kenny.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 
+ * @Title: IMOOCJSONResult.java
+ * @Package com.kenny.utils
+ * @Description: Custom response data structure
+ * This class can be provided to H5/ios/Android/WeChat Official Account/Mini Program
+ * After receiving this type of data (JSON object), the frontend can implement related functions based on business logic
+ * 200: Indicates success
+ * 500: Indicates an error, with the error message in the msg field
+ * 501: Bean validation error, returns all errors in a map format
+ * 502: Interceptor intercepts user token error
+ * 555: Exception throw information
+ * 556: User qq verification exception
+ * @Copyright: Copyright (c) 2020
+ * @Company: www.kenny.com
+ * @author Kenny
+ * @version V1.0
+ */
+public class IMOOCJSONResult {
+
+    // Define jackson object
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    // Response business status
+    private Integer status;
+
+    // Response message
+    private String msg;
+
+    // Response data
+    private Object data;
+    
+    @JsonIgnore
+    private String ok;	// Not used
+
+    public static IMOOCJSONResult build(Integer status, String msg, Object data) {
+        return new IMOOCJSONResult(status, msg, data);
+    }
+
+    public static IMOOCJSONResult build(Integer status, String msg, Object data, String ok) {
+        return new IMOOCJSONResult(status, msg, data, ok);
+    }
+    
+    public static IMOOCJSONResult ok(Object data) {
+        return new IMOOCJSONResult(data);
+    }
+
+    public static IMOOCJSONResult ok() {
+        return new IMOOCJSONResult(null);
+    }
+    
+    public static IMOOCJSONResult errorMsg(String msg) {
+        return new IMOOCJSONResult(500, msg, null);
+    }
+    
+    public static IMOOCJSONResult errorMap(Object data) {
+        return new IMOOCJSONResult(501, "error", data);
+    }
+    
+    public static IMOOCJSONResult errorTokenMsg(String msg) {
+        return new IMOOCJSONResult(502, msg, null);
+    }
+    
+    public static IMOOCJSONResult errorException(String msg) {
+        return new IMOOCJSONResult(555, msg, null);
+    }
+    
+    public static IMOOCJSONResult errorUserQQ(String msg) {
+        return new IMOOCJSONResult(556, msg, null);
+    }
+
+    public IMOOCJSONResult() {
+
+    }
+
+    public IMOOCJSONResult(Integer status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+    
+    public IMOOCJSONResult(Integer status, String msg, Object data, String ok) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+        this.ok = ok;
+    }
+
+    public IMOOCJSONResult(Object data) {
+        this.status = 200;
+        this.msg = "OK";
+        this.data = data;
+    }
+
+    public Boolean isOK() {
+        return this.status == 200;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+	public String getOk() {
+		return ok;
+	}
+
+	public void setOk(String ok) {
+		this.ok = ok;
+	}
+
+}
---
+++ common/foodie-cloud-shared-pojo/src/main/java/com/kenny/pojo/PagedGridResult.java
@@ -0,0 +1,43 @@
+package com.kenny.pojo;
+
+import java.util.List;
+
+/**
+ * 
+ * @Title: PagedGridResult.java
+ * @Package com.imooc.utils
+ * @Description: Used to return the data format for paginated Grid
+ * Copyright: Copyright (c) 2019
+ */
+public class PagedGridResult {
+	
+	private int page;			// Current page number
+	private int total;			// Total number of pages
+	private long records;		// Total number of records
+	private List<?> rows;		// Content displayed in each row
+
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public long getRecords() {
+		return records;
+	}
+	public void setRecords(long records) {
+		this.records = records;
+	}
+	public List<?> getRows() {
+		return rows;
+	}
+	public void setRows(List<?> rows) {
+		this.rows = rows;
+	}
+}
