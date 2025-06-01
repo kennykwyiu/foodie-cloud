a6b5627ff07425b40d9dcf9c7b71f69a6f0b1c1d
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-01 15:46:32 +0800

    add ItemCommentService

---
+++ domain/item/foodie-item-api/src/main/java/com/kenny/item/service/ItemCommentService.java
@@ -0,0 +1,23 @@
+package com.kenny.item.service;
+
+import com.kenny.pojo.PagedGridResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequestMapping("item-comments-api")
+public interface ItemCommentService {
+    /**
+     * Query my comments with pagination
+     * @param userId
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("myComments")
+    public PagedGridResult queryMyComments(@RequestParam("userId") String userId, 
+                                         @RequestParam(value = "page", required = false) Integer page, 
+                                         @RequestParam(value = "pageSize", required = false) Integer pageSize);
+    @PostMapping("saveComments")
+    public void saveComments(@RequestBody Map<String,Object> map);
+}
