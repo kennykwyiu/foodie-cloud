cbe32d5a8fef3763e31fc45cbd33ca92988ee8e3
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-04 20:52:26 +0800

    create UserAddressMapper.java

---
+++ domain/user/foodie-user-mapper/src/main/java/com/kenny/user/mapper/UserAddressMapper.java
@@ -0,0 +1,8 @@
+package com.kenny.user.mapper;
+
+
+import com.kenny.my.mapper.MyMapper;
+import com.kenny.user.pojo.UserAddress;
+
+public interface UserAddressMapper extends MyMapper<UserAddress> {
+}
