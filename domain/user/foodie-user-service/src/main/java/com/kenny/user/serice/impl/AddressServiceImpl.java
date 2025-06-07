78569f3edbfd94c53b20e5041bac8125f6cb5e08
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-07 14:26:40 +0800

    add AddressServiceImpl

---
+++ domain/user/foodie-user-service/src/main/java/com/kenny/user/serice/impl/AddressServiceImpl.java
@@ -0,0 +1,120 @@
+package com.kenny.user.serice.impl;
+
+import com.kenny.enums.YesOrNo;
+import com.kenny.user.mapper.UserAddressMapper;
+import com.kenny.user.pojo.UserAddress;
+import com.kenny.user.pojo.bo.AddressBO;
+import com.kenny.user.service.AddressService;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private UserAddressMapper userAddressMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<UserAddress> queryAll(String userId) {
+
+        UserAddress ua = new UserAddress();
+        ua.setUserId(userId);
+
+        return userAddressMapper.select(ua);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void addNewUserAddress(AddressBO addressBO) {
+
+        // 1. Check if the current user has any addresses, if not, set as 'default address'
+        Integer isDefault = 0;
+        List<UserAddress> addressList = this.queryAll(addressBO.getUserId());
+        if (addressList == null || addressList.isEmpty() || addressList.size() == 0) {
+            isDefault = 1;
+        }
+
+        String addressId = sid.nextShort();
+
+        // 2. Save address to database
+        UserAddress newAddress = new UserAddress();
+        BeanUtils.copyProperties(addressBO, newAddress);
+
+        newAddress.setId(addressId);
+        newAddress.setIsDefault(isDefault);
+        newAddress.setCreatedTime(new Date());
+        newAddress.setUpdatedTime(new Date());
+
+        userAddressMapper.insert(newAddress);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void updateUserAddress(AddressBO addressBO) {
+
+        String addressId = addressBO.getAddressId();
+
+        UserAddress pendingAddress = new UserAddress();
+        BeanUtils.copyProperties(addressBO, pendingAddress);
+
+        pendingAddress.setId(addressId);
+        pendingAddress.setUpdatedTime(new Date());
+
+        userAddressMapper.updateByPrimaryKeySelective(pendingAddress);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void deleteUserAddress(String userId, String addressId) {
+
+        UserAddress address = new UserAddress();
+        address.setId(addressId);
+        address.setUserId(userId);
+
+        userAddressMapper.delete(address);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void updateUserAddressToBeDefault(String userId, String addressId) {
+
+        // 1. Find default address and set it as non-default
+        UserAddress queryAddress = new UserAddress();
+        queryAddress.setUserId(userId);
+        queryAddress.setIsDefault(YesOrNo.YES.type);
+        List<UserAddress> list  = userAddressMapper.select(queryAddress);
+        for (UserAddress ua : list) {
+            ua.setIsDefault(YesOrNo.NO.type);
+            userAddressMapper.updateByPrimaryKeySelective(ua);
+        }
+
+        // 2. Update the specified address to be default
+        UserAddress defaultAddress = new UserAddress();
+        defaultAddress.setId(addressId);
+        defaultAddress.setUserId(userId);
+        defaultAddress.setIsDefault(YesOrNo.YES.type);
+        userAddressMapper.updateByPrimaryKeySelective(defaultAddress);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public UserAddress queryUserAddres(String userId, String addressId) {
+
+        UserAddress singleAddress = new UserAddress();
+        singleAddress.setId(addressId);
+        singleAddress.setUserId(userId);
+
+        return userAddressMapper.selectOne(singleAddress);
+    }
+}
